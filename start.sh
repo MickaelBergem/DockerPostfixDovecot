61a25347a33954d7811865316a567619f14530b2
Mickaël Bergem <mickael@securem.eu>
2016-09-15 19:46:25 +0200

    Fix hostname issue

--- start.sh
+++ start.sh
@@ -12,9 +12,9 @@ export HOST_IP=$(/sbin/ip route|awk '/default/ { print $3 }')
 echo "$HOST_IP dockerhost" >> /etc/hosts
 
 # defining mail name
-echo $DB_HOST > /etc/mailname
+echo $APP_HOST > /etc/mailname
 
-postconf -e myhostname=$DB_HOST
+postconf -e myhostname=$APP_HOST
 
 # update config templates
 sed -i "s/{{DB_USER}}/$DB_USER/g" /etc/postfix/mysql-email2email.cf
